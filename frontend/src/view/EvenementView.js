85d019adb4217910a7ba1131881c1e87bb1f00d4
lys yoyo <162428987+lalberick@users.noreply.github.com>
2024-07-05 12:58:05 +0200

    zqqswaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ frontend/src/view/EvenementView.js
@@ -0,0 +1,12 @@
+import Description from "../components/Description/Description";
+
+
+const EvenementView = () => {
+    return(
+        <div>
+            <Description/>
+        </div>
+    )
+}
+
+export default EvenementView
