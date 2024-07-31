2d36edc99368089cd8cdfdcc0b1eeb0cac244525
Tamen Paternus <85977527+tamen23@users.noreply.github.com>
2024-07-31 09:39:34 +0200

    fix:storing event

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,4 +4,12 @@ const api = axios.create({
   baseURL: 'http://localhost:8000/api',
 });
 
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token'); // or however you're storing the token
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
 export default api;
